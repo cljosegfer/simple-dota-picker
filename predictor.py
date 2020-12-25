59fed475bdd5bf78062a2b502f1ba15a853c9964
josegfer <macaverde604@gmail.com>
2020-12-25 09:20:10 -0300

    added hero name dictionary

--- predictor.py
+++ predictor.py
@@ -2,16 +2,17 @@ import pandas as pd
 import numpy as np
 
 from favorite_heroes import formal_names
+from hero_dictionary import dictionary
 
 reference_table = pd.read_csv('reference_table.csv')
 
-enemy_team = ['Enigma', 
-              'Medusa', 
-              'Phoenix', 
-              'Storm Spirit', 
-              'Windranger']
-for i in range(len(enemy_team)):
-    enemy_team[i] = input("Enter the enemy hero: ")
+enemy_team = ['enigma', 
+              'dusa', 
+              'phoenix', 
+              'storm', 
+              'wind']
+# for i in range(len(enemy_team)):
+#     enemy_team[i] = input("Enter the enemy hero: ")
 
 decision_table = np.zeros((len(enemy_team), reference_table.shape[1] - 1))
 
@@ -19,9 +20,11 @@ i = 0
 j = 0
 for enemy in enemy_team:
     box = np.zeros((1, len(formal_names) + 1))
-    for hero in reference_table['Hero']:
-        if enemy == hero:
-            box = reference_table[reference_table['Hero']==enemy].to_numpy()
+    for hero in dictionary:
+        for hero_name in hero:
+            if enemy == hero_name:
+                enemy = hero[0]
+                box = reference_table[reference_table['Hero']==enemy].to_numpy()
     decision_table[i, :] = box[:, 1:]
     i+=1
     j+=1
